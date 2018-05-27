18e743df3720a4478979b2a16eb629aaaa4c70a4
Zhenyu Wu <Adam_5Wu@hotmail.com>
2018-05-27 01:38:21 -0400

    Update bearssl headers

--- src/include/bearssl_port.h
+++ src/include/bearssl_port.h
@@ -5,12 +5,16 @@
 extern "C" {
 #endif
 
+#ifdef ESP8266_ALTSTACK
+
 #include <stdint.h>
 
 extern void br_esp8266_stack_proxy_init(uint8_t *space, uint16_t size);
 extern size_t br_esp8266_stack_proxy_max();
 extern size_t br_esp8266_stack_proxy_usage();
 extern void br_esp8266_stack_proxy_deinit();
+
+#endif
 
 #ifdef __cplusplus
 };
